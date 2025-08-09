18692e194606e98d9bc2b56a7dde527a547e923d
abdelkaderbouzmita@takiacademyteam.com <abdelkaderbouzmita@takiacademyteam.com>
2025-08-09 17:21:05 +0100

    Refactor OnboardingScreen and SocialAuth components to improve code clarity
    - Removed unused font import in OnboardingScreen for cleaner code.
    - Cleaned up imports in SocialAuth component for better readability.

--- src/components/auth/SocialAuth.tsx
+++ src/components/auth/SocialAuth.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Pressable, View, Image } from 'react-native';
 import Svg, { Path } from 'react-native-svg';
-import AppText from '../../../components/AppText';
+
 
 const SocialAuth: React.FC = () => {
   return (
